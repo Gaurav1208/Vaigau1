8f98d6cd24e859a9df89ab52515de762771692ef
Gaurav1208 <43243098+Gaurav1208@users.noreply.github.com>
2019-07-27 19:38:53 +0530

    set price on spinner

--- app/src/main/java/com/example/vaigau/treatment.java
+++ app/src/main/java/com/example/vaigau/treatment.java
@@ -2,12 +2,19 @@ package com.example.vaigau;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class treatment extends AppCompatActivity {
     Spinner sp;
+    TextView tv;
+    Button b;
     String treatment[]={"Threading","Forehead","Upper-Lip","Chin-Thread","Face-Thread-Side","Nose Thread","Full-Face-Thread"};
+    String price[]={"20","15","36","76","65","76","76","98"};
     ArrayAdapter<String> adp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,30 @@ public class treatment extends AppCompatActivity {
         sp = findViewById(R.id.spintreat);
         adp=new ArrayAdapter<String>(treatment.this,android.R.layout.simple_list_item_1,treatment);
         sp.setAdapter(adp);
+        b= findViewById(R.id.bt);
+        tv = findViewById(R.id.tv);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                abc(price[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                int n=sp.getSelectedItemPosition();
+//                abc(price[n]);
+            }
+        });
+    }
+    public void abc(String s){
+        tv.setText(s);
+
     }
 }
